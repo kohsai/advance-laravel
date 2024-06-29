c9afe75ca6166ef08c2cb2b2ebda84888032e6b4
kohsai <saishokoudai@gmail.com>
2024-06-29 18:40:12 +0900

    commit

---
+++ src/routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [AuthorController::class, 'index']);
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
+Route::get('/author/{author}', [AuthorController::class, 'bind']);
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'create']);
+Route::get('/edit', [AuthorController::class, 'edit']);
+Route::post('/edit', [AuthorController::class, 'update']);
+Route::get('/delete', [AuthorController::class, 'delete']);
+Route::post('/delete', [AuthorController::class, 'remove']);
+Route::get('/relation', [AuthorController::class, 'relate']);
+
+Route::prefix('book')->group(function () {
+    Route::get('/', [BookController::class, 'index']);
+    Route::get('/add', [BookController::class, 'add']);
+    Route::post('/add', [BookController::class, 'create']);
+    Route::get('/session', [SessionController::class, 'getSes']);
+    Route::post('/session', [SessionController::class, 'postSes']);
+
+    Route::get('/softdelete', function () {
+        Person::find(1)->delete();
+    });
+    Route::get('softdelete/absolute', function () {
+        $result = Person::onlyTrashed()->forceDelete();
+        echo ($result);
+    });
+    Route::get('softdelete/get', function () {
+        $person = Person::onlyTrashed()->get();
+        dd($person);
+    });
+    Route::get('softdelete/store', function () {
+        $result = Person::onlyTrashed()->restore();
+        echo $result;
+    });
+
+});
+Route::get('uuid', function () {
+    $products = Product::all();
+    foreach ($products as $product) {
+        echo $product . '<br>';
+    }
+});
